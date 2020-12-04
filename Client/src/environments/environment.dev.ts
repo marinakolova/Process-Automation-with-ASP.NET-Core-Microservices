ae1c42fbdbd08c3c0e4c0b4925903cb74d560780
marinakolova <marina.kolova.359@gmail.com>
2020-12-04 19:01:17 +0200

    Added Kubernetes files and updated environments

---
+++ Client/src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    identityApiUrl: 'http://35.184.214.91:5001/',
+    dealersApiUrl: 'http://34.70.174.159:5002/',
+    statisticsApiUrl: 'http://34.69.213.76:5003/',
+    notificationsUrl: 'http://35.232.221.138:5004/'
+  };
